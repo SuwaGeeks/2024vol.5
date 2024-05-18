89dee3da5dc581c2c5eb62de1cd3738c7c68f2f0
Kento210 <kentokentokento913@gmail.com>
2024-05-18 14:28:49 +0900

    fix: routerの設定を見直しました

--- keyguardian-app/src/App.js
+++ keyguardian-app/src/App.js
@@ -1,19 +1,15 @@
-import './App.css';
-import Home from "./Home";
-import Start from "./Start";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Start from "./pages/Start";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
         <Routes>
           <Route path="/" element={<Start />} />
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<h1>Not Found Page</h1>} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
