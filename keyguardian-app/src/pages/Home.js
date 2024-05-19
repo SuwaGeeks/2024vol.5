04ea088ae01acef8882c26fcd73a1f0521808b76
kaketo <kaketo.suwageeks@gmail.com>
2024-05-19 12:41:18 +0900

    開放ページを作成しました．

--- keyguardian-app/src/pages/Home.js
+++ keyguardian-app/src/pages/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
+import styles from "./Home.css"
+import key from './key.png'
+import opening from './opening.png'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div>
+            <header className="Home-header">
+                <img src={key} alt="key logo" className="header-Image" />
+                <h1 className="header-title">KeyGuardian</h1>
+            </header>
+            <main className="Home-main">
+                    <h2 className="main-message">
+                        ようこそKeyGuardianへ<br/>
+                        現在の○○研究室の開閉状態は
+                    </h2>
+                    <div className="opening">
+                        <img src={opening} alt="opening icon" className="opening-Image" />
+                        <p className="opening-txt">開放</p>
+                    </div>
+                </main>
+        </div>
     )
 }
 
