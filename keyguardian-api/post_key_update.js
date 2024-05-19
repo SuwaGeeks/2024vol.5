786741c4b372f6e364318b6497fa114be29e8e68
TNKNMKHI <nazo161018@gmail.com>
2024-05-19 12:15:20 +0900

    デバッグ

--- keyguardian-api/post_key_update.js
+++ keyguardian-api/post_key_update.js
@@ -1,3 +1,4 @@
+
 // AWS SDKのインポート
 const AWS = require('aws-sdk');
 
@@ -8,33 +9,37 @@ const handler = async (event) => {
     // レスポンスデータの作成
     const item = event.body;
 
+    // レスポンスデータをbase64でデコード
+    let item_decode = atob(item);
     try {
-        // レスポンスデータをbase64でデコード
-        let item_decode = Buffer.from(item, 'base64').toString('utf-8');
-        
-        // デコードされた文字列をログに出力
-        console.log('Decoded string:', item_decode);
-        
-        // デコードされた文字列をJSONオブジェクトに変換
         item_decode = JSON.parse(item_decode);
-        
-        // DynamoDBにデータを書き込むためのパラメータの設定
-        const params = {
-            TableName: 'keyGuadian_DB',
-            Item: item_decode,
+    } catch(error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('json error'),
         };
+    }
+    
 
-        // DynamoDBにデータを書き込む
+    // DynamoDBにデータを書き込むためのパラメータの設定
+    const params = {
+        TableName: 'keyGuadian_DB',
+        Item: item_decode,
+    };
+
+    // DynamoDBにデータを書き込む
+    try {
         await dynamoDB.put(params).promise();
         return {
             statusCode: 200,
             body: JSON.stringify('Data written to DynamoDB successfully'),
         };
     } catch (error) {
-        console.error('Error:', error);
+        console.error(error);
         return {
             statusCode: 500,
-            body: JSON.stringify(error),
+            body: JSON.stringify('An error occurred while writing data to DynamoDB'),
         };
     }
 };
