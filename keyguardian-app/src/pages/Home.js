41336787ba291fc638d18a85431ac4626f9cd25f
kaketo <kaketo.suwageeks@gmail.com>
2024-05-19 14:32:06 +0900

    タイムスタンプを表示させました．

--- keyguardian-app/src/pages/Home.js
+++ keyguardian-app/src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useLocation } from 'react-router-dom';
 import styles from "./Home.css"
 import key from './key.png'
-import opening from './opening.png'
+import { Opening } from './opening.js'
+import { Closed } from './closed.js'
 
 const Home = () => {
+    const [isOpened, setIsOpened] = useState(false);
+    const url = useLocation().pathname;
+    const timestamp = new Date("2024-05-17T12:34:56Z");
+    
+    function TimeEdit(timestamp){
+        const date = timestamp
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        return `${year}/${month}/${day} ${hours}:${minutes}`;
+    }
+
+    const time = TimeEdit(timestamp)
+
     return (
         <div>
             <header className="Home-header">
@@ -15,10 +34,7 @@ const Home = () => {
                         ようこそKeyGuardianへ<br/>
                         現在の○○研究室の開閉状態は
                     </h2>
-                    <div className="opening">
-                        <img src={opening} alt="opening icon" className="opening-Image" />
-                        <p className="opening-txt">開放</p>
-                    </div>
+                    {isOpened ? <Opening time={time}/> : <Closed time={time}/>}
                 </main>
         </div>
     )
